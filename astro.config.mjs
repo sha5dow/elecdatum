39fd2eb712eba393c640e89a61e8757ca57afb7a
Sergio Martinez <mpse90@gmail.com>
2026-07-27 13:17:17 -0600

    fix astro.config.mjs syntax (powershell backtick escaping)

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,12 +43,12 @@ export default defineConfig({
         },
         {
           tag: 'script',
-          content: \
+          content: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-RC5WSLWH5E');
-          \,
+          `,
         },
       ],
       components: {
